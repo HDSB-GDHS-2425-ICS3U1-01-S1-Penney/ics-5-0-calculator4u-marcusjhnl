71e928369105391f29e40df57164f80a81f5a620
marcusjhnl <1spraguemar@hdsb.ca>
2024-09-20 12:46:13 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstnumber;
+let secondnumber;
+let operator;
+
+operator = prompt(" operator ")
+firstnumber = prompt("first number")
+secondnumber = prompt("second number")
+
+console.log(operator);
+console.log(firstnumber);
+console.log(secondnumber);
