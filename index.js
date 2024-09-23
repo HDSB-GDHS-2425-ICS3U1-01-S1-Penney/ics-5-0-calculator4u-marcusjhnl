adaa1e1fe6e8b1983be324be660dea26c9fbddb6
marcusjhnl <1spraguemar@hdsb.ca>
2024-09-23 12:45:22 -0400

    unfinished

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ secondnumber = prompt("second number")
 console.log(operator);
 console.log(firstnumber);
 console.log(secondnumber);
+
+function calculator() {
+
+    if (operator == "multiply") {
+        console.log(firstnumber * secondnumber);
+    } else if (operator == "divide") {
+        console.log(firstnumber / secondnumber)
+        if (operator = 0)
+        console.log("undefined")
+    } else if (operator == "subtract") {
+        console.log(firstnumber - secondnumber)
+    } else if (operator == "add") {
+        console.log(firstnumber + secondnumber)
+    }
+}
+
+calculator(firstnumber * secondnumber);
